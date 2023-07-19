30bda95eda56a7141ec6f53ba970c153b82b89d7
DESKTOP-5KNSTRC\yjcom <yjdocument7@gmail.com>
2023-07-19 10:35:41 +0900

    문제 5번

---
+++ workshop05/src/student/student.java
@@ -0,0 +1,36 @@
+package student;
+
+public class student {
+
+	private String name;
+	private int korean;
+	private int math;
+	private int science;
+	
+	public student() {}
+	
+	public student(String name, int korean, int math, int science) {
+		super();
+		this.name = name;
+		this.korean = korean;
+		this.math = math;
+		this.science = science;
+	}
+	
+	public double getAvg() {
+		return 0.0;
+	}
+	
+	public String getGrade() {
+		return null;
+	}
+	
+	public void setName(String name) {
+		this.name = name
+	}
+	
+
+	
+	
+
+}
