ca65a42b1c3d2f549231336df896131c45eeca4d
DESKTOP-5KNSTRC\yjcom <yjdocument7@gmail.com>
2023-07-19 10:35:02 +0900

    자바문법

--- JAVA_Grammer/src/Java_Grammer_Package/Cat.java
+++ JAVA_Grammer/src/Java_Grammer_Package/Cat.java
@@ -1,5 +1,22 @@
 package Java_Grammer_Package;
 
+// 현실세계의 고양이 객체에 해당하는 Cat 클래스
+// main 메서드가 없다.
+
 public class Cat {
 
+	// 인스턴스 변수(멤버 변수): 객체속성값 저장
+	String name;
+	int age;
+	String sex;
+	
+	
+	
+	// 생성자 (comstructor): 인스턴스 변수 초기화
+	
+	// 메서드(멤버메서드): 인스턴스 변수 관리
+	
+	
+	
+	
 }
