5a086983c001b0edf9609b4dedfa3dbb2e92652c
DESKTOP-5KNSTRC\yjcom <yjdocument7@gmail.com>
2023-07-19 22:01:59 +0900

    예제 1

--- JAVA_Grammer/src/Java_Grammer_Package/exam01.java
+++ JAVA_Grammer/src/Java_Grammer_Package/exam01.java
@@ -3,8 +3,15 @@ package Java_Grammer_Package;
 public class exam01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+
+			// 포맷 출력
+			// 1. String.format(String 형식문자, Object ... 값들) // Object 의미는 모든 값의미
+			String result = String.format("이름:%s, 나이:%d, 키:%f, 성별:%c, 결혼:%b", "홍길동", 20, 167.2389652, '남', true);
+			System.out.println(result);
+			
+			System.out.printf("이름:%s, 나이:%d, 키:%.2f, 성별:%c, 결혼:%b", "홍길동", 20, 167.2389652, '남', true);
+			System.out.printf("이름:%s, 나이:%d, 키:%f, 성별:%c, 결혼:%b", "홍길동", 20, 167.2389652, '남', true);
 	}
 
 }
